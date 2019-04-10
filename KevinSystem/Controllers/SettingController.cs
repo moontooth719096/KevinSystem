5f3d8d3c8532cebb51f2fa3bd7a86b7e622a6632
GuoWeiWu <moontooth719096@gmail.com>
2019-04-10 22:08:17 +0800

    新增頁面

---
+++ KevinSystem/Controllers/SettingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KevinSystem.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult FunctionSettingPage()
+        {
+            return View();
+        }
+    }
+}
